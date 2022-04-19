e4aca532365635914c34a47ecd097ca4ba36d03c
Kshitij Raj <kr91323@gmail.com>
2022-04-19 16:38:07 +0530

    main

---
+++ compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+read -p 'enter file location:' dri
+
+if [[ -f "$dri"  && $dri == *.c ]] 
+then
+echo "File exists"
+read -p "file : " file
+read -p "output :" op
+gcc $file -o  $op
+read -p "output directory :" opdri
+mv $op $opdri
+elif [[ -f "$dri" && $dri == *.cpp ]] 
+then
+echo "File exists"
+read -p "file : " file
+read -p "output :" op
+g++ $file -o  $op
+read -p "output directory :" opdri
+mv $op $opdri
+else
+echo "Error"
+fi
+
+
+
+	
+
