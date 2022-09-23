33ea0321f40b0defcef12d8cb88094e21bb79f64
Kshitij Raj <kr91323@gmail.com>
2022-09-23 12:36:35 +0530

    main

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p 'enter file location:' dri
 
-if [[ -f "$dri"  && $dri == *.c ]] 
+if [[ -f $dri  && $dri == *.c ]] 
 then
 echo "File exists"
 read -p "file : " file
@@ -9,7 +9,7 @@ read -p "output :" op
 gcc $file -o  $op
 read -p "output directory :" opdri
 mv $op $opdri
-elif [[ -f "$dri" && $dri == *.cpp ]] 
+elif [[ -f $dri && $dri == *.cpp ]] 
 then
 echo "File exists"
 read -p "file : " file
